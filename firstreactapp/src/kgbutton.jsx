f119ae5406eb6dea15eca7f774a386a156ffd2e8
dhara-maru <dharamaru406@gmail.com>
2024-10-08 19:01:24 +0530

    removing firstreactapp directory from here

--- firstreactapp/src/kgbutton.jsx
+++
@@ -1,7 +0,0 @@
-function KGButton(){
-    return <button type="button" class="btn btn-primary">
-    Click Me
-  </button>
-}
-
-export default KGButton;
--- firstreactapp/src/random.jsx
+++
@@ -1,9 +0,0 @@
-function Random(){
-    let random = Math.random()* 100;
-
-    return <h1 style={{'background': '#345233',
-        'font-size':'20px'
-    }}>Random number is {Math.round(random)}</h1>
-}
-
-export default Random;
