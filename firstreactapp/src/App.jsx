a0f43ce8aec62438b913d48716b96c186a7fbf62
dhara-maru <dharamaru406@gmail.com>
2024-10-08 19:05:08 +0530

    adding firstreactapp to .gitignore

---
+++ firstreactapp/src/App.jsx
@@ -0,0 +1,22 @@
+import KGButton from "./kgbutton";
+import Hello from "./hello";
+import Random from "./random";
+
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Hello World 
+      </h1>
+
+<button type="button" class="btn btn-dark">Dark</button>
+      <KGButton></KGButton>
+      <Hello></Hello>
+      <Random></Random>
+   
+    </div>
+  );
+}
+
+export default App;
---
+++ firstreactapp/src/kgbutton.jsx
@@ -0,0 +1,7 @@
+function KGButton(){
+    return <button type="button" class="btn btn-primary">
+    Click Me
+  </button>
+}
+
+export default KGButton;
