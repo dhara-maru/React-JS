9fd03441dffdc2f31c94a82e2b6a4cc655b07ee5
dhara-maru <dharamaru406@gmail.com>
2024-10-10 19:57:17 +0530

    React Clock UI (Version 1)

--- clockreact/src/components/currentTime.jsx
+++ clockreact/src/components/currentTime.jsx
@@ -2,7 +2,7 @@ let CurrentTime = () => {
     let time = new Date();
     return <>
     <p>Current time is : {time.toLocaleTimeString()}</p>
-    <p>Current Date is : {time.toLocaleDateString()}</p>
+    <p>Current Date is {time.toLocaleDateString()}</p>
 
     </>
     };
