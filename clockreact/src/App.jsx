3fbd0cba8932d27fd461d08aeb246e1cbba28740
dhara-maru <dharamaru406@gmail.com>
2024-10-14 18:49:48 +0530

    map small intro

--- clockreact/src/App.jsx
+++ clockreact/src/App.jsx
@@ -4,14 +4,16 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import ClockHeading from './components/clockHeading'
 import ClockSlogan from './components/clockSlogan'
 import CurrentTime from './components/currentTime'
+import Frag from './topicsfromtutorial/fragments'
 function App() {
  
   return (
     <>
       
-      <ClockHeading></ClockHeading>
+      {/* <ClockHeading></ClockHeading>
       <ClockSlogan></ClockSlogan>
-      <CurrentTime></CurrentTime>
+      <CurrentTime></CurrentTime> */}
+      <Frag></Frag>
     </>
   )
 }
