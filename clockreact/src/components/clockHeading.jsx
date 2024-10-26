fdacc77904a838f7fa173e27f818fe7d08e5f57e
dhara-maru <dharamaru406@gmail.com>
2024-10-26 13:54:05 +0530

    removed the optionals, tuts ended

--- clockreact/src/components/clockHeading.jsx
+++
@@ -1,4 +0,0 @@
-let ClockHeading = () => {
-    return <h1>My Clock</h1>
-};
-export default ClockHeading;
--- clockreact/src/components/clockSlogan.jsx
+++
@@ -1,4 +0,0 @@
-let ClockSlogan = () => {
-    return <p>Stay in sync with the present moment ! : React Clock</p>
-};
-export default ClockSlogan;
